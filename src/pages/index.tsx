4aad8084385a6b38f3c3a30fff51e4b1d2c97fcd
Francesco Masala <mail@francescomasala.me>
2023-02-06 18:43:03 +0100

    Added plausible header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,8 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+        <script defer data-domain="docs.hyperbit.it" src="https://plausible.io/js/script.js"></script>
+        <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
